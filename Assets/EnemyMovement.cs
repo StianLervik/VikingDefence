4ed16824df12e61c3a2f8f8e86f0bf5d1521ddf2
StianLervik <stian.lervik@outlook.com>
2016-08-30 22:04:53 +0200

    VikingDefence
    Lasta opp spelert.

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public int enemySpeed;
+
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate (){
+		Vector2 movement = new Vector2 (-1 * enemySpeed, body.velocity.y);
+		body.velocity = movement;
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public int playerSpeed;
+	public int airSpeed;
+	public int jumpStrength;
+	public int maxSpeed;
+
+	private Rigidbody2D body;
+	private Vector2 rayBox;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		rayBox = new Vector2 (0.9f, 0.9f);
+	}
+
+	void FixedUpdate (){
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		RaycastHit hit;
+		if (Physics2D.BoxCast (transform.position, rayBox, 0.0f, -transform.up, 0.12f)) {
+			Vector2 movement = new Vector2 (horizontal * playerSpeed, body.velocity.y);
+			if (body.velocity.x < maxSpeed) {
+				body.AddForce (movement);
+			}
+			if (Input.GetKey ("w")) {
+				body.AddForce (transform.up * jumpStrength);
+			}
+		} else {
+			Vector2 movement = new Vector2 (horizontal * airSpeed, body.velocity.y);
+			if (body.velocity.magnitude < maxSpeed) {
+				body.AddForce (movement);
+			}
+		}
+	}
+}
